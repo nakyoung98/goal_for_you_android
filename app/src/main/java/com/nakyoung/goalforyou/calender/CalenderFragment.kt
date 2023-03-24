6fe4d6a6d16a4261a76b36df762667a50882252b
이나경 <66313756+nakyoung98@users.noreply.github.com>
2023-03-24 18:08:42 +0900

    [MOD] CalenderFragment viewModel추가
    CalenderViewModel을 가지도록 함

--- app/src/main/java/com/nakyoung/goalforyou/calender/CalenderFragment.kt
+++ app/src/main/java/com/nakyoung/goalforyou/calender/CalenderFragment.kt
@@ -1,25 +1,34 @@
 package com.nakyoung.goalforyou.calender
 
+import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import com.nakyoung.goalforyou.R
 import com.nakyoung.goalforyou.databinding.FragmentCalenderBinding
 
-class CalenderFragment: Fragment(){
+class CalenderFragment : Fragment() {
 
-    private var _binding: FragmentCalenderBinding? = null
-    val binding
-        get() = _binding!!
+    companion object {
+        fun newInstance() = CalenderFragment()
+    }
 
+    private lateinit var viewModel: CalenderViewModel
+    private lateinit var binding: FragmentCalenderBinding
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentCalenderBinding.inflate(inflater,container,false)
+        binding = FragmentCalenderBinding.inflate(inflater,container,false)
         return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this)[CalenderViewModel::class.java]
+        // TODO: Use the ViewModel
     }
 
 }
