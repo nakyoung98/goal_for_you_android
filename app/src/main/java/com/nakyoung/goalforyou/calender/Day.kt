e57cb99c30acc28775af7652c68b908da63f2a00
이나경 <66313756+nakyoung98@users.noreply.github.com>
2023-03-24 18:26:29 +0900

    [MOD] Day.kt Week eum 삭제

--- app/src/main/java/com/nakyoung/goalforyou/calender/Day.kt
+++ app/src/main/java/com/nakyoung/goalforyou/calender/Day.kt
@@ -3,16 +3,6 @@ package com.nakyoung.goalforyou.calender
 import com.nakyoung.goalforyou.database.domain.Goal
 import java.time.LocalDate
 
-enum class Week {
-    SUNDAY,
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY
-}
-
 data class Day (
     val day: Int,
     val goals: List<Goal>,
