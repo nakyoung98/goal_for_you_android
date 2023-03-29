6723c47e1f8cda1bddc95ffc9745ad47d47a4ada
이나경 <66313756+nakyoung98@users.noreply.github.com>
2023-03-29 18:29:54 +0900

    [ADD] CalenderUtil 추가
    lengthOfMonth,
    lengthOfThisMonth,
    firstDayOfWeek
    기능 신설

---
+++ app/src/main/java/com/nakyoung/goalforyou/calender/CalenderUtil.kt
@@ -0,0 +1,40 @@
+package com.nakyoung.goalforyou.calender
+
+import java.time.DayOfWeek
+import java.time.LocalDate
+
+class CalenderUtil {
+
+    companion object{
+
+        val today = LocalDate.now()
+
+        /**
+         * 해당 월의 총 날짜를 반환
+         * 금년을 기준으로 함
+         **/
+        fun lengthOfMonth(month: Int): Int {
+            val tempMonth = LocalDate.of(today.year,month,1).month
+            return tempMonth.length(today.isLeapYear)
+        }
+
+        /**
+         * 이번 달의 총 날짜를 반환
+         * 금년을 기준으로 함
+         **/
+        fun lengthOfThisMonth(): Int {
+            val tempMonth = LocalDate.of(today.year, today.month,1).month
+            return tempMonth.length(today.isLeapYear)
+        }
+
+        /**
+         * 해당 월 1일의 요일을 반환
+         * 금년을 기준으로 함
+         **/
+        fun firstDayOfWeek(month: Int): DayOfWeek {
+            return LocalDate.of(today.year,today.month,1).dayOfWeek
+        }
+
+    }
+
+}
