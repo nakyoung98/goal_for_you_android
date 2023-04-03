6ef34999f70bd2484633e6d8e50fbccec68d4515
이나경 <66313756+nakyoung98@users.noreply.github.com>
2023-04-03 17:39:58 +0900

    [MOD] nav_graph_main.xml 새 action 추가
    calenderDetailDialog DialogFragment로 이동하는 action 추가
    
    calenderFragment -> calenderDetailDialog

---
+++ app/src/main/java/com/nakyoung/goalforyou/calender/CalenderDetailDialog.kt
@@ -0,0 +1,48 @@
+package com.nakyoung.goalforyou.calender
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.setFragmentResultListener
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.navGraphViewModels
+import com.nakyoung.goalforyou.R
+import com.nakyoung.goalforyou.databinding.DialogCalenderDetailBinding
+import kotlinx.coroutines.launch
+
+class CalenderDetailDialog( val indexIndays: Int
+        ): DialogFragment() {
+
+    private lateinit var binding: DialogCalenderDetailBinding
+
+    private val viewModel: CalenderViewModel by navGraphViewModels(R.id.nav_graph_main)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        binding = DialogCalenderDetailBinding.inflate(layoutInflater)
+
+        lifecycleScope.launch {
+            binding.day.text = viewModel!!.days[indexIndays].day.toString()
+        }
+
+        return AlertDialog.Builder(requireContext())
+            .setView(binding.root)
+            .setNegativeButton("확인") { dialog, id -> getDialog()?.cancel() }
+            .create()
+    }
+}
