c57451ef3f0b8845fa76093c9b11eeb70c9edc88
nakyoung98 <66313756+nakyoung98@users.noreply.github.com>
2023-04-06 02:01:22 +0900

    .

--- app/src/main/java/com/nakyoung/goalforyou/main/goal/GoalRecyclerView.kt
+++ app/src/main/java/com/nakyoung/goalforyou/main/goal/GoalRecyclerView.kt
@@ -1,37 +1,60 @@
 package com.nakyoung.goalforyou.main.goal
 
 import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.nakyoung.goalforyou.R
+import com.nakyoung.goalforyou.databinding.RecyclerviewGoalBinding
 import com.nakyoung.goalforyou.main.database.domain.Goal
 import com.nakyoung.goalforyou.view.GoalView
+import java.util.concurrent.atomic.AtomicInteger
 
-class GoalRecyclerView(
-    context: Context,
-    layoutManager: LayoutManager? = LinearLayoutManager(context),
-    val data: List<Goal>): RecyclerView(context) {
+
+class GoalRecyclerView
+    @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyle: Int = com.google.android.material.R.attr.recyclerViewStyle,
+        layoutManager: LayoutManager? = LinearLayoutManager(context),
+        data: List<Goal>? = null
+        ): RecyclerView(context, attrs, defStyle) {
+
+    val binding
+        get() = _binding!!
+    private var _binding: RecyclerviewGoalBinding? = null
+
     init {
         setLayoutManager(layoutManager)
         adapter = GoalViewAdapter()
+        _binding = RecyclerviewGoalBinding.inflate(LayoutInflater.from(context))
     }
+
 }
 
 class GoalViewAdapter() : RecyclerView.Adapter<GoalViewHolder>() {
+    val count: AtomicInteger = AtomicInteger(1)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GoalViewHolder {
-        TODO("Not yet implemented")
+        return GoalViewHolder(GoalView(parent.context))
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return 100
     }
 
     override fun onBindViewHolder(holder: GoalViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(count.getAndIncrement())
     }
 
 }
 
-class GoalViewHolder(goalView: GoalView) : RecyclerView.ViewHolder(goalView) {
+class GoalViewHolder(val goalView: GoalView) : RecyclerView.ViewHolder(goalView) {
+    val count: AtomicInteger = AtomicInteger(1)
 
+    fun bind(count: Int) {
+        goalView.binding.goalTitle.text = "" + count + " & " + this.count.getAndIncrement() + ""
+    }
 }
