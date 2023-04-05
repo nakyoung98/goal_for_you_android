9590ff8b673bdeaae92c6311feb57a734656736b
이나경 <66313756+nakyoung98@users.noreply.github.com>
2023-04-05 18:30:26 +0900

    [ADD] GoalView 커스텀 뷰 생성

---
+++ app/src/main/java/com/nakyoung/goalforyou/view/GoalView.kt
@@ -0,0 +1,27 @@
+package com.nakyoung.goalforyou.view
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.RectShape
+import android.graphics.drawable.shapes.Shape
+import android.util.AttributeSet
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.nakyoung.goalforyou.R
+import com.nakyoung.goalforyou.databinding.GoalViewBinding
+
+class GoalView
+@JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0, defStyleRes: Int = 0)
+    : ConstraintLayout(context, attrs, defStyleAttr, defStyleRes)
+{
+    private val binding: GoalViewBinding
+
+    init {
+        binding = GoalViewBinding.inflate(LayoutInflater.from(context),this,true)
+    }
+}
+
