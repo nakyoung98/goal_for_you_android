2497975e69b35aca556551b2644f7dd8fae52cc6
이나경 <66313756+nakyoung98@users.noreply.github.com>
2023-04-06 18:38:03 +0900

    [MOD] GoalRecyclerView 수정
    GoalItemDecoration만 수정하면 완료됨

--- app/src/main/java/com/nakyoung/goalforyou/main/goal/GoalRecyclerView.kt
+++ app/src/main/java/com/nakyoung/goalforyou/main/goal/GoalRecyclerView.kt
@@ -1,16 +1,24 @@
 package com.nakyoung.goalforyou.main.goal
 
 import android.content.Context
+import android.graphics.PixelFormat
+import android.graphics.Rect
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.nakyoung.goalforyou.R
 import com.nakyoung.goalforyou.databinding.RecyclerviewGoalBinding
 import com.nakyoung.goalforyou.main.database.domain.Goal
+import com.nakyoung.goalforyou.util.GraphicUtil
 import com.nakyoung.goalforyou.view.GoalView
-import java.util.concurrent.atomic.AtomicInteger
 
 
 class GoalRecyclerView
@@ -19,42 +27,74 @@ class GoalRecyclerView
         attrs: AttributeSet? = null,
         defStyle: Int = com.google.android.material.R.attr.recyclerViewStyle,
         layoutManager: LayoutManager? = LinearLayoutManager(context),
-        data: List<Goal>? = null
         ): RecyclerView(context, attrs, defStyle) {
 
     val binding
         get() = _binding!!
     private var _binding: RecyclerviewGoalBinding? = null
 
+    var data: List<Goal>? = null
+        set(value) {
+            adapter = GoalViewAdapter(value!!)
+            field = value}
+
     init {
         setLayoutManager(layoutManager)
-        adapter = GoalViewAdapter()
         _binding = RecyclerviewGoalBinding.inflate(LayoutInflater.from(context))
     }
 
+    fun paddingRight() {
+        addItemDecoration(GoalItemDecoration(context))
+    }
 }
 
-class GoalViewAdapter() : RecyclerView.Adapter<GoalViewHolder>() {
-    val count: AtomicInteger = AtomicInteger(1)
+class GoalViewAdapter(private val data: List<Goal>) : RecyclerView.Adapter<GoalViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GoalViewHolder {
-        return GoalViewHolder(GoalView(parent.context))
+        return GoalViewHolder(GoalView(parent.context).apply {
+
+        })
     }
 
     override fun getItemCount(): Int {
-        return 100
+        return data.size
     }
 
     override fun onBindViewHolder(holder: GoalViewHolder, position: Int) {
-        holder.bind(count.getAndIncrement())
+        holder.bind(data[position])
     }
 
 }
 
 class GoalViewHolder(val goalView: GoalView) : RecyclerView.ViewHolder(goalView) {
-    val count: AtomicInteger = AtomicInteger(1)
 
-    fun bind(count: Int) {
-        goalView.binding.goalTitle.text = "" + count + " & " + this.count.getAndIncrement() + ""
+    fun bind(goal: Goal) {
+        Log.i("GoalViewHolder","bind")
+        goalView.binding.goalStatus.text = GoalUtil.getStatus(goal).name
+        goalView.binding.goalTitle.text = goal.goalContent
+
+        val leftDDay = GoalUtil.getDDay(goal.goalFinish)
+        goalView.binding.goalDDay.text = GoalUtil.DDaytoString(leftDDay)
+
+        goalView.binding.goalProgressBar.setProgressStatus(goal)
+    }
+}
+
+class GoalItemDecoration(context: Context) : RecyclerView.ItemDecoration() {
+    var padding = context.resources.getDimensionPixelSize(R.dimen.margin_xsmall)
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+
+        val layoutManager = parent.layoutManager
+
+        if ( layoutManager is GridLayoutManager) {
+            parent.getChildAdapterPosition(view)
+            layoutManager.spanCount
+        }
     }
 }
