b0b141617d9d9b89d67d9ef0439154a097e76312
이나경 <66313756+nakyoung98@users.noreply.github.com>
2023-04-06 18:34:34 +0900

    [MOD] CalenderDetailDialog 수정
     viewmodel 이름 정정

--- app/src/main/java/com/nakyoung/goalforyou/main/calender/CalenderDetailDialog.kt
+++ app/src/main/java/com/nakyoung/goalforyou/main/calender/CalenderDetailDialog.kt
@@ -2,21 +2,12 @@ package com.nakyoung.goalforyou.main.calender
 
 import android.app.AlertDialog
 import android.app.Dialog
-import android.content.Context
-import android.content.DialogInterface
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.setFragmentResultListener
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navGraphViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.nakyoung.goalforyou.R
 import com.nakyoung.goalforyou.databinding.DialogCalenderDetailBinding
 import kotlinx.coroutines.launch
@@ -26,23 +17,21 @@ class CalenderDetailDialog( val indexIndays: Int
 
     private lateinit var binding: DialogCalenderDetailBinding
 
-    private val viewModel: CalenderViewModel by navGraphViewModels(R.id.nav_graph_main)
+    private val calenderViewModel: CalenderViewModel by navGraphViewModels(R.id.nav_graph_main)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-
         binding = DialogCalenderDetailBinding.inflate(layoutInflater)
 
         lifecycleScope.launch {
-            binding.day.text = viewModel.days[indexIndays].day.toString()
+            binding.day.text = calenderViewModel.days[indexIndays].day.toString()
+            binding.goalRecyclerView.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+            binding.goalRecyclerView.data = calenderViewModel.days[indexIndays].goals
         }
-
         return AlertDialog.Builder(requireContext())
             .setView(binding.root)
             .setNegativeButton("확인") { _, _ -> dialog?.cancel() }
             .create()
     }
+
+
 }
