63e6e7eac3c30ca64d4a3224d0c1132fd070c789
이나경 <66313756+nakyoung98@users.noreply.github.com>
2023-04-11 10:31:31 +0900

    [MOD] calender view가 정상적으로 표시되지 않는 문제 해결
    wrap_content -> match_content

--- app/src/main/java/com/nakyoung/goalforyou/view/CalenderView.kt
+++ app/src/main/java/com/nakyoung/goalforyou/view/CalenderView.kt
@@ -2,7 +2,9 @@ package com.nakyoung.goalforyou.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.nakyoung.goalforyou.databinding.CalenderViewBinding
 import com.nakyoung.goalforyou.databinding.ProgressbarGoalBinding
@@ -11,8 +13,10 @@ class CalenderView @JvmOverloads constructor(context: Context, attrs: AttributeS
     : ConstraintLayout(context, attrs, defStyleAttr, defStyleRes) {
 
     val binding: CalenderViewBinding
+    private val TAG = "CalenderView"
 
     init {
-        binding = CalenderViewBinding.inflate(LayoutInflater.from(context),this,true)
+        Log.i(TAG, "init")
+        binding = CalenderViewBinding.inflate(LayoutInflater.from(context), this, true)
     }
 }
